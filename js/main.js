e1ccc2659bdfc42b9be6ac1ec6751f7d9a06f0cc
gmluqa <gmluqa@protonmail.com>
2022-10-07 13:16:01 +0200

    Definiendo la clase Persona

---
+++ js/main.js
@@ -0,0 +1,11 @@
+class Persona {
+    constructor(nombre = "Pepe", edad = 30, dni, sexo = "m", peso = 70, altura = "1,8m") {
+        this.nombre = nombre
+        this.edad = edad
+        this.dni = dni
+        this.sexo = sexo
+        this.peso = peso
+        this.altura = altura
+    }
+}
+
