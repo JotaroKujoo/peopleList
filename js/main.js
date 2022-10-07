847e2019a833af51911020edee065b0bb621cfca
Jose Rodriguez <rdrgz.jose123@gmail.com>
2022-10-07 13:27:57 +0200

    Comentando codigo del reto finalizado y empezando siguiente ejercicio

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-class Persona {
-    constructor(dni, nombre = "Pepe", edad = 30, sexo = "m", peso = 70, altura = "1,8m") {
-        this.nombre = nombre
-        this.edad = edad
-        this.dni = dni
-        this.sexo = sexo
-        this.peso = peso
-        this.altura = altura
-    }
-}
+// class Persona {
+//     constructor(dni, nombre = "Pepe", edad = 30, sexo = "m", peso = 70, altura = "1,8m") {
+//         this.nombre = nombre
+//         this.edad = edad
+//         this.dni = dni
+//         this.sexo = sexo
+//         this.peso = peso
+//         this.altura = altura
+//     }
+// }
 
-let persona1 = new Persona("73816732Y")
-let persona2 = new Persona("73434232Z")
-let persona3 = new Persona("37816732O")
+// let persona1 = new Persona("73816732Y")
+// let persona2 = new Persona("73434232Z")
+// let persona3 = new Persona("37816732O")
